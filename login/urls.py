2e83b7dbd3b0a47966388223702095d9e61311ae
VictorRH9 <victor13900@gmail.com>
2025-04-26 10:35:46 -0600

    Update urls.py

--- login/urls.py
+++ login/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
-from .views import login_view, logout_view, inicio_view, empleados_view, calculo_view, historial_view, anticipos_view, configuracion_view
+from .views import login_view, logout_view
 
 urlpatterns = [
-    path('login/',  login_view,  name='login'),
+    path('', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('',           inicio_view,        name='inicio'),
-    path('empleados/', empleados_view,     name='empleados'),
-    path('calculo/',    calculo_view,      name='calculo'),
-    path('historial/',  historial_view,    name='historial'),
-    path('anticipos/',  anticipos_view,    name='anticipos'),
-    path('configuracion/', configuracion_view, name='configuracion'),
 ]
